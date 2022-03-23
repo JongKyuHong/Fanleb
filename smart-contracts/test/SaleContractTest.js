be89f172b8c326671b5cc911b7759dab26b8658c
SSAFY <kongi7064@gmail.com>
2022-03-23 11:20:57 +0900

    feat: test

--- smart-contracts/test/SaleContractTest.js
+++ smart-contracts/test/SaleContractTest.js
@@ -45,26 +45,39 @@ contract("Sale Contract Testing", (accounts) => {
     //     // assert.equal(900, await getBalance(purchaser), "Transfer Failed");
     // });
 
-    it("Purchase", async () => {
+    it("Purchase1", async () => {
         const seller = accounts[0];
         const purchaser = accounts[1];
-        SsafyToken.contract(instance=>{
-            instance.mint(mintAmount);
-            instance.forceToTransfer(seller,purchaser,1000);
+        return SsafyToken.deployed("ssafytoken","ssf","10000")
+            .then(instance =>{
+                instance.mint(mintAmount) // 테스트위한 임의의 erc20 토큰 생성 후 10,000토큰 발행
+                instance.forceToTransfer(seller,purchaser,1000) // 구매자 주소로 1,000토큰 부여
         })
-        SsafyNFT.contract(instance=>{
-            const id = instance.create(seller, uri);  // 마감일 빼고서 작업하자
-            SaleFactory.contract(instance2=>{
-                instance2.createSale(id,100,0,1);
-            })
-            Sale.contract(instance3=>{
-                instance3.purchase();
-            })
-        })
+    });
+    it("Purchase2", async() =>{
         
+        return SsafyNFT.deployed()
+            .then(instance =>{
+                id = instance.create(seller,uri)
+            })
+    });
+    it("Purchase3", async() =>{
+        return SaleFactory.deployed()
+            .then(instance=>{
+                instance.creaetSale(id, 100,0x6C927304104cdaa5a8b3691E0ADE8a3ded41a333,0x077A32620d00a1257E82D542969567Ac6e13f7cB)
+                instance.purchase()
+            })
+    });
 
-
-
+        // SsafyNFT.contract(instance=>{
+        //     const id = instance.create(seller, uri);  // 마감일 빼고서 작업하자
+        //     SaleFactory.contract(instance2=>{
+        //         instance2.createSale(id,100,0,1);
+        //     })
+        //     Sale.contract(instance3=>{
+        //         instance3.purchase();
+        //     })
+        // })
         // const tokeninstance = await SsafyToken.deployed("ssafytoken","ssf","10000");
         // tokeninstance.mint(mintAmount);
         // tokeninstance.forceToTransfer(seller,purchaser,1000);
@@ -120,17 +133,14 @@ contract("Sale Contract Testing", (accounts) => {
         //         instance.creaetSale(id, 100, time, time2,0x6C927304104cdaa5a8b3691E0ADE8a3ded41a333,0x077A32620d00a1257E82D542969567Ac6e13f7cB)
         //         instance.purchase()
         //     })
+        it("Bid and Cancel", async () => {
+            const seller = accounts[0];
+            const bidder = accounts[1];
+    
+    
+            // TODO
+            // 다음을 테스트를 통과해야합니다.
+            // assert.equal(seller, await getNftOwner(), "Cancellation Failed");
+            // assert.equal(1000, await getBalance(bidder), "Refund Failed");
+        });
     });
-
-    it("Bid and Cancel", async () => {
-        const seller = accounts[0];
-        const bidder = accounts[1];
-
-
-        // TODO
-        // 다음을 테스트를 통과해야합니다.
-        // assert.equal(seller, await getNftOwner(), "Cancellation Failed");
-        // assert.equal(1000, await getBalance(bidder), "Refund Failed");
-    });
-
-});
