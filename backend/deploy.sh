f4b5d4b72958a8270b36f0647d60b2d5fac492c1
JMine97 <52027965+JMine97@users.noreply.github.com>
2022-03-25 19:55:08 +0900

    Feat: deploy

---
+++ backend/deploy.sh
@@ -0,0 +1,9 @@
+PROC=`ps aux | grep app`
+if [ $PROC == *"app"* ]; then
+    echo "Process is running."
+    sudo kill -15 `ps -ef | grep app | grep -v grep | awk '{print $2}'`
+else
+    echo "Process is not running."
+fi
+
+nohup java -jar /home/ubuntu/app.jar &
