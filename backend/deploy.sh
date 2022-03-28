f2f207e30b04c67e39233f0dd6c84a0da4bfd332
JMine97 <52027965+JMine97@users.noreply.github.com>
2022-03-28 19:05:50 +0900

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,12 +7,13 @@ echo "Running PID: {$CURRENT_PID}"
 if [ -z $CURRENT_PID ] ; then 
    echo "Project is not running"
 else
-   kill -9 $CURRENT_PID 
+   echo "Project is running"
+   sudo kill -9 $CURRENT_PID
    sleep 10 
 fi 
 
 echo "Deploy Project...." 
-nohup java -jar /home/ubuntu/spring.jar >> /home/ubuntu/spring.log &
+sudo nohup java -jar /home/ubuntu/spring.jar >> /home/ubuntu/spring.log &
 
-echo $(pgrep -f app*)
+echo $(pgrep -f spring*)
 echo "Done"
