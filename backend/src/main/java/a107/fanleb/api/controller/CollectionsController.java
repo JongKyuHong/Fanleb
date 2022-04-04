d11510c7531e5dba42f5ed4a5ee1a581c3d5f07a
JMine97 <52027965+JMine97@users.noreply.github.com>
2022-04-04 13:26:40 +0900

    Fix: 컬렉션 검색

--- backend/src/main/java/a107/fanleb/api/controller/CollectionsController.java
+++ backend/src/main/java/a107/fanleb/api/controller/CollectionsController.java
@@ -30,7 +30,7 @@ public class CollectionsController {
     @GetMapping("/list")
     public ResponseEntity<? extends BaseResponseBody> showList(@RequestParam(required = false, name="search[query]") String query,
                                                                @RequestParam(required = false, name="search[sortAscending]") String isAscending,
-                                                               @RequestParam(required = false, name="search[sortBy]", defaultValue = "singer") String sortedBy,
+                                                               @RequestParam(required = false, name="search[sortBy]") String sortedBy,
                                                                @RequestParam(value = "page", defaultValue = "1") int page) {
         return ResponseEntity.status(200).body(AdvancedResponseBody.of("success", collectionsService.showList(page, query, isAscending, sortedBy)));
     }
