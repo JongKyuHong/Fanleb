cb9c96d4a4a251293f18692a1c022e8de3ffd2d4
JYA <abogado2113@gmail.com>
2022-04-05 04:47:46 +0900

    Refactor: 네비게이션바 검색창  입력시 정보가  잘 출력되도록 코드 수정

--- frontend/src/pages/SearchResult.js
+++ frontend/src/pages/SearchResult.js
@@ -6,94 +6,97 @@ import Post from "../components/post/Post";
 import Posts from "../components/post/Posts";
 import Skeleton from "../components/skeleton/skeleton";
 import queryString from "query-string";
+import InfiniteScroll from "react-infinite-scroll-component";
 const SearchResult = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [posts, setPosts] = useState([]);
   const location = useLocation();
   const query = queryString.parse(location.search)
+  const [totalCount, setTotalCount] = useState(0);
   const onItemClicked = () => {
     alert('클릭!')
   }
-  // const handleScroll = (e) => {     
-  //   const scrollHeight = e.target.documentElement.scrollHeight;
-  //   const currentHeight = Math.ceil(
-  //   e.target.documentElement.scrollTop + window.innerHeight
-  //   );
-  //   if (currentHeight + 200 >= scrollHeight) {
-  //     setIsLoading(true);
-  //     getData('singer')
-  //     // if (value === 0) {
-  //     //     getData('singer')
-  //     //   } else if (value === 1) {
-  //     //     getData('actor');
-  //     //   } else if (value === 2) {
-  //     //     getData('celeb');
-  //     //   } else if (value === 3) {
-  //     //     getData('general');
-  //     //   }     
-  //   }
-  // };
-  const getData = async () => {
-    const category = ['singer', 'actor', 'celeb', 'general'];
-    let collectionsData = [];
-    for (let i = 0; i < category.length; i++) {
-      let currentPage = 1;
-      console.log(category[i])
-      const { data } = await axios(
-        {
-          method: "GET",
-          url: `api/collections/list?search[sortBy]=${category[i]}&search[query]=${query.query}&page=${currentPage}`,
-          headers: {}
-        })
-      console.log(data)
-      if (currentPage === data.data.total_pages) {
-        console.log('종료')
-        collectionsData.concat(data.data.content)
-        currentPage++;
-        // setPosts(old => {        
-        //   return [...old, ...data.data.content]
-        // })
-        // setIsLoading(false)        
-      } else if (currentPage === data.data.total_pages + 1) {
-        console.log('특수 케이스 종료')
-      } else {
-        // setPosts(oldData => [...oldData, ...data.data.content])
-        // setIsLoading(false)
-        collectionsData.concat(data.data.content)
-        console.log('채우기')
-        currentPage++;
-      }
-    }
-    console.log(collectionsData)
-    setPosts(collectionsData);
-  }   
 
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(2);
+  const fetchCollections = async () => {
+    try {
+      const res = await axios(
+        `api/collections/list?search[sortBy]=&search[query]=&page=${page}`
+        )
+      return res.data.data.content;
+    } catch (err) {      
+      return 'err'
+    }
+  };
+
+  const fetchData = async () => {
+    const collectionsFromServer = await fetchCollections();
+    if (collectionsFromServer === 'err') {
+      setHasMore(false)
+      return
+    }
+    // const collectionsFromServer = res.data.data.content
+    // if (posts.length === 0 || posts.length < 12) {
+    //   setHasMore(false)
+    //   return
+    // }
+    if (posts.length === 0 || posts.length < 12 || collectionsFromServer.length === 0 || collectionsFromServer.length < 12) {
+      setPosts([...posts, ...collectionsFromServer]);
+      setHasMore(false)
+      // return
+    } else {
+      setPosts([...posts, ...collectionsFromServer]);
+      setPage(page + 1);    
+    }
+  };
   useEffect(() => {
-    getData()    
+    const getFirstData = async () => {
+      setPosts([]);
+      setPage(2)
+      setHasMore(true)
+      const { data } = await axios(`api/collections/list?search[sortBy]=&search[query]=${query.query}&page=1`);
+      if (data.data.last) {
+        setHasMore(false)
+      }
+      console.log(data)
+      setTotalCount(data.data.total_elements)
+      setPosts(data.data.content);
+    };
+    getFirstData();
     return () => {
-      setPosts([])      
+      setPosts([]);
     }
   }, [])
   return (
     // <Page title="SSAFY NFT" maxWidth="100%" minHeight="100%" alignItems="center" display="flex">
     <Page title="FANLEB" sx={{}}>
-      
-      <h3 style={{color: 'white', margin: '10vh 20vh'}}>items</h3>
+      <div style={{marginTop: '20vh', marginLeft: '10vw', marginBottom: '3vw'}}>
+        <h2 style={{ color: 'white' }}>{ totalCount } Items</h2>
+      </div>
       <div style={{paddingBottom: '10vh'}}>
         <div className='bids'>
-          <div className="bids-container">          
-            {/* <div className="bids-container-text">
-              <h1>Hot</h1>
-            </div> */}
-            {isLoading ? <div className="bids-container-card"><Skeleton type="post" /></div>
-              :
-              <div className="bids-container-card">
-                {posts.map((post, idx) => <Post key={idx} post={post} onItemClicked={onItemClicked} />)}
-              </div>
-            }
+          <div className="bids-container">
+            <div className="bids-container-card">
+              <InfiniteScroll
+                dataLength={posts?.length} //This is important field to render the next data
+                next={fetchData}
+                hasMore={hasMore}
+                loader={
+                  <Skeleton type="post" />  
+                }
+                // endMessage={
+                //   <div style={{color: 'white'}}>
+                //     <b>Yay! You have seen it all</b>
+                //   </div>
+                // }
+                >              
+                  {posts?.map((post, idx) => <Post key={idx} post={post} onItemClicked={onItemClicked} />)}              
+              </InfiniteScroll> 
+            </div>
           </div>
         </div>
-      </div>     
+      </div>    
     </Page>
   );
 };
