5e40eaaaf7f15a50c9bc322d6898829a00d9cbf0
JMine97 <52027965+JMine97@users.noreply.github.com>
2022-04-05 16:51:08 +0900

    Feat: 랭킹 페이지

---
+++ backend/src/main/java/a107/fanleb/api/service/RankingService.java
@@ -0,0 +1,25 @@
+package a107.fanleb.api.service;
+
+import a107.fanleb.api.response.ranking.RankingListViewRes;
+import a107.fanleb.domain.RankingRepoistorySuppot;
+import a107.fanleb.domain.contents.Contents;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class RankingService {
+
+    private final RankingRepoistorySuppot rankingRepoistorySuppot;
+
+    @Transactional(readOnly = true)
+    public Page<RankingListViewRes> view(int page) {
+        PageRequest pageable = PageRequest.of(page - 1, 10);
+
+        return rankingRepoistorySuppot.view(pageable);
+    }
+}
