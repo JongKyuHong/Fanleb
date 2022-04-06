46bf055d1ac8b033300615a3f877a8d9897b00fd
JYA <abogado2113@gmail.com>
2022-04-06 22:02:56 +0900

    Feat: 썸네일 페이지 구독하기 및 입장하기 기능, CSS

--- frontend/src/components/ThumnailModal/ThumnailModal.js
+++ frontend/src/components/ThumnailModal/ThumnailModal.js
@@ -1,19 +1,39 @@
 import { Divider, Modal } from '@mui/material';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { removeThumnail, toggleModal } from '../../redux/modalSlice';
 import './ThumnailModal.css';
 import empty from "../post/empty-image.jpg";
 import { useNavigate } from 'react-router-dom';
+import { getSubscriptionInfo, SubscribeUser } from '../../utils/Subscription';
 function ThumnailModal() {
-  const { isOpen, collectionName, thumnailImgUrl, userImgUrl, nickname, description, category, userAddress, contentsData} = useSelector(state => state.modal);
+  const { isOpen, collectionName, thumnailImgUrl, userImgUrl, nickname, description, category, userAddress, contentsData } = useSelector(state => state.modal);
+  
   const dispatch = useDispatch();
-  const addr = useSelector(state => state.user.userInfo.userAddress)
+  const myAddr = useSelector(state => state.user.userInfo.userAddress)
+  const [subscriptionCnt, setSubscriptionsCnt] = useState(0);
+  const [isSubscribed, setIssubscribed] = useState(false);
   const navigator = useNavigate();
   const closeModal = () => {
     dispatch(removeThumnail())
     dispatch(toggleModal())
   }
+  const subscribe = async () => {
+    await SubscribeUser(userAddress, myAddr, setSubscriptionsCnt);
+
+  }  
+  useEffect(() => {    
+    const getData = async () => {
+      const {count, status} = await getSubscriptionInfo(userAddress, myAddr);
+      
+      setSubscriptionsCnt(count)
+      setIssubscribed(status)
+    };
+    if (isOpen) {
+      getData();
+      console.log('모달 정보', userImgUrl, description)
+    }
+  }, [isOpen])
   return (
     <Modal
       open={isOpen}
@@ -28,38 +48,45 @@ function ThumnailModal() {
         <div className="item-image">
           <img src={thumnailImgUrl ? thumnailImgUrl : empty} alt="item" />
         </div>
-          <div className="item-content">
-            <div className="item-content-title">
-              <h1>{collectionName}</h1>
-              <p>From <span>4.5 SSF</span> ‧ 20 of 25 available</p>
-            </div>
-            <div className="item-content-creator">
-            <div style={{overflow: 'hidden'}}><p>{ category }</p></div>
-              <div>
-                <img src={userImgUrl} alt="creator" />
-                <p>{nickname} </p>             
-              </div>              
-            </div>
-            <div className="item-content-detail">
-            <p>{description}</p>
-            <Divider sx={{margin: '20px 0'}} />
-              <p style={{overflow: 'hidden', width: 'auto'}}>Address: {userAddress}</p>
-            </div>
-            <div className="item-content-buy">
-              <button className="primary-btn" onClick={() => alert('준비중')}>구독하기 4.5 ETH</button>
+        <div className="item-content">
+          <div className="item-content-title">
+            <h1>{collectionName}</h1>
+            <p><span>1 SSF</span> ‧ {subscriptionCnt} available</p>
+          </div>
+          <div className="item-content-creator">
+          <div style={{overflow: 'hidden'}}><p>{ category }</p></div>
+            <div>
+              <img src={userImgUrl} alt="creator" />
+              <p>{nickname} </p>             
+            </div>              
+          </div>
+        <div className="item-content-detail">
+          {/* <h3 style={{color: 'white'}}>소개</h3> */}
+          <p>{description}</p>
+          <Divider sx={{margin: '20px 0'}} />
+          <p style={{ overflow: 'hidden', width: 'auto' }}> <span style={{color: 'rgb(32, 129, 226)'}}>{ nickname } </span>님의 Address : {userAddress}</p>
+        </div>
+        <div className="item-content-buy">
+          {!isSubscribed ?
+          
+            <button className="primary-btn" onClick={() => subscribe()}>구독하기 ( 1 SSF )</button>
+            :
             <button className="secondary-btn"
               onClick={() => {
-                if (addr) {
-                  closeModal()
-                  navigator(`/content/${userAddress}`)
-                } else {
-                  alert('지갑 연결이 필요합니다.')
-                  window.open('https://metamask.io/download/', '_blank')
-                }
+                closeModal()
+                navigator(`/content/${userAddress}`)
+                // if (addr) {
+                //   closeModal()
+                //   navigator(`/content/${userAddress}`)
+                // } else {
+                //   alert('지갑 연결이 필요합니다.')
+                //   window.open('https://metamask.io/download/', '_blank')
+                // }
               }}
-              >입장하기</button>
-            </div>
+            >입장하기</button>
+          }
           </div>
+        </div>
       </div>
     </Modal>
   )
