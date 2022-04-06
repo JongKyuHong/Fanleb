f395840ebadad53c819f01d7e9159251e79d7aef
홍종규 <kongi4289@naver.com>
2022-04-06 07:57:53 +0000

    Update SaleInfoGetter.js

--- frontend/src/common/SaleInfoGetter.js
+++ frontend/src/common/SaleInfoGetter.js
@@ -17,7 +17,7 @@ export default async function getByTokenId(id) {
   axios(config)
   .then(function (response) {
     //console.log(JSON.stringify(response.data));
-    return [response.data.sale_contract_address, response.data.seller_address];
+    return response.data.sale_contract_address;
   })
   .catch(function (error) {
     console.log(error);
