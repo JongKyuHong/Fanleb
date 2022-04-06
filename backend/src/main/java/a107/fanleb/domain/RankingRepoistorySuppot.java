1ab1490eefc82d8d69337a580124e4b79ec597e4
JMine97 <52027965+JMine97@users.noreply.github.com>
2022-04-06 10:03:02 +0900

    Feat: 구독자, 구독하는 사람 표시

--- backend/src/main/java/a107/fanleb/domain/RankingRepoistorySuppot.java
+++ backend/src/main/java/a107/fanleb/domain/RankingRepoistorySuppot.java
@@ -1,9 +1,7 @@
 package a107.fanleb.domain;
 
-import a107.fanleb.api.response.collections.CollectionsListViewRes;
 import a107.fanleb.api.response.ranking.QRankingListViewRes;
 import a107.fanleb.api.response.ranking.RankingListViewRes;
-import a107.fanleb.domain.collections.QCollections;
 import a107.fanleb.domain.contents.QContents;
 import a107.fanleb.domain.sales.QSales;
 import a107.fanleb.domain.users.QUsers;
@@ -27,15 +25,18 @@ public class RankingRepoistorySuppot {
     QContents qContents = QContents.contents;
 
     
-    //todo : recent traded price 수정
+    //todo : 인원 여러명 일 때 왜 안 나오누..
+    //max, intvalue 쪽이 문제인듯???
     public Page<RankingListViewRes> view(Pageable pageable) {
-        List<RankingListViewRes> fetch = jpaQueryFactory.select(new QRankingListViewRes(qUsers.nickname, qUsersCategory.userCategoryName, qUsers.curSubscribeCnt, qUsers.contentsCnt, qSales.price.doubleValue(), qSales.count().intValue(), qUsers.imgUrl))
+        List<RankingListViewRes> fetch = jpaQueryFactory.select(new QRankingListViewRes(qUsers.nickname, qUsersCategory.userCategoryName, qUsers.curSubscribeCnt, qUsers.contentsCnt, qSales.price.max(), qSales.count().intValue(), qUsers.imgUrl))
                 .from(qUsers)
                 .leftJoin(qUsersCategory).on(qUsers.usersCategory.eq(qUsersCategory))
                 .leftJoin(qContents).on(qContents.ownerAddress.eq(qUsers.userAddress))
                 .leftJoin(qSales).on(qSales.tokenId.eq(qContents.tokenId))
                 .where(qSales.saleYn.eq(Status.y))
                 .offset(pageable.getOffset()).limit(pageable.getPageSize()).fetch();
+
+        System.out.println(fetch);
 
         int id = (int) pageable.getOffset()+1;
 
@@ -44,6 +45,8 @@ public class RankingRepoistorySuppot {
             r.setId(id);
             id++;
         }
+
+        System.out.println(fetch);
 
         return new PageImpl<>(fetch, pageable, fetch.size());
     }
