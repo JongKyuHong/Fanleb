3c0409505630e8203e97305d32144d528e1afca5
홍종규 <kongi4289@naver.com>
2022-04-06 03:10:56 +0000

    feat: CurrencyAddress

--- frontend/src/common/AddressStore.js
+++ frontend/src/common/AddressStore.js
@@ -3,7 +3,8 @@ const AddressStore = {
     // 문자열로 바꿔줘야 인식이 됩니다. SsafyNFT만 작동 확인했습니다.
     SsafyNFT: ['0x2A45463dD4423E3309FAe7ac08b37e8557252c7E'],
     SsafyToken : ['0xa792F53C2A4379b2Fe2AFF0690732aCC756EC2Fd'],
-    SaleFactory : ['0xa61CCf42A3fC1D17DB37AcE2b8E21A1d73b33A92']
+    SaleFactory : ['0xa61CCf42A3fC1D17DB37AcE2b8E21A1d73b33A92'],
+    CurrencyAddress : ['0x6C927304104cdaa5a8b3691E0ADE8a3ded41a333'],
   }
 }
 
